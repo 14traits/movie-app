8608358f2f7e0b2c27bdba4f3bdf6cd196b9639e
John Rogers <john.rogers@mac.com>
2022-06-30 17:11:16 -0500

    ++actors_controler, find_actor method, and route.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+actor = Actor.new({ first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
+actor.save
+
+# ingore this I was cheating ....
+# _______________________________________________________
+# actor = Actor.new(first_name: "Johnny", last_name: "Depp", known_for: "Pirates of the Carribean")
+# actor.save
+# actor = Actor.new(first_name: "Robert", last_name: "Downey, Jr", known_for: "Ironman")
+# actor.save
+# actor = Actor.new(first_name: "Matt", last_name: "Damon", known_for: "The Martian")
+# actor.save
+# actor = Actor.new(first_name: "Daniel", last_name: "Craig", known_for: "007")
+# actor.save
+# actor = Actor.new(first_name: "Hugh", last_name: "Jackman", known_for: "Wolverine")
+# actor.save
