3e6364a37bf3b86ce985e568bc9edc67b860f97c
John Rogers <john.rogers@mac.com>
2022-07-05 19:49:01 -0500

    homework from params homework

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,25 @@
 class ActorsController < ApplicationController
+  def index
+    array = Array.new
+    actors = Actor.all
+    actors.each do |actor|
+      array << { id: actor.id, first_name: actor.first_name, last_name: actor.last_name, known_for: actor.known_for }
+    end
+    render json: array.as_json
+  end
+
   def find_actor
     actor = Actor.find_by(id: params["id"])
+    render json: actor.as_json
+  end
+
+  def last_name_find
+    actor = Actor.find_by(last_name: params["last_name"])
+    render json: actor.as_json
+  end
+
+  def search
+    actor = Actor.find_by(last_name: params["last_name"])
     render json: actor.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  # Actors
+  # Get
+  get "/actors" => "actors#index"
   get "/find/:id", controller: "actors", action: "find_actor"
+  get "/actors/:last_name" => "actors#last_name_find"
+  # Post
+  post "/find/actor/" => "actors#search"
 
+  # Movies
   get "/movies" => "movies#index"
   get "/movies/:id" => "movies#show"
 end
