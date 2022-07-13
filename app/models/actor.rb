9deab22fe5c8b87bb905fc85e0f44dfc0d6129cd
John Rogers <john.rogers@mac.com>
2022-07-12 23:19:00 -0500

    ++ accocations to actor and movie models, migration to add movie_id to actors ᕙ(⇀‸↼‶)ᕗ

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,6 +1,7 @@
 class Actor < ApplicationRecord
-  validates :first_name, presence: true, length: { minimum: 2 }
-  validates :last_name, presence: true, length: { minimum: 2 }
-  validates :known_for, presence: true
-  validates :age, presence: true, comparison: { greater_than_or_equal_to: 13 }
+  belongs_to :movie
+  # validates :first_name, presence: true, length: { minimum: 2 }
+  # validates :last_name, presence: true, length: { minimum: 2 }
+  # validates :known_for, presence: true
+  # validates :age, presence: true, comparison: { greater_than_or_equal_to: 13 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,10 @@
 class Movie < ApplicationRecord
-  validates :title, presence: true, length:
-  validates :director, presence: true, length: { minimum: 2 }
-  validates :plot, presence: true
-  # now to make sure its after the first movie ever filmed:
-  # 1888. In Leeds, England Louis Le Prince films Roundhay Garden Scene, believed to be the first motion picture recorded.  https://en.wikipedia.org/wiki/List_of_cinematic_firsts
-  validates :year, presence: true, comparison: { greater_than_or_equal_to: 1888 }
+  has_many :actors
+  # validates :title, presence: true, length:
+  # validates :director, presence: true, length: { minimum: 2 }
+  # validates :plot, presence: true
+  # # now to make sure its after the first movie ever filmed:
+  # # 1888. In Leeds, England Louis Le Prince films Roundhay Garden Scene, believed to be the first motion picture recorded.  https://en.wikipedia.org/wiki/List_of_cinematic_firsts
+  # validates :year, presence: true, comparison: { greater_than_or_equal_to: 1888 }
 
 end
