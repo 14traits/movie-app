2e1ebd2509e967d2990f7f031bc5d420b035bd85
John Rogers <john.rogers@mac.com>
2022-07-16 16:02:55 -0500

    auth part2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :actors, :movies, defaults: { format: :json }
 
-  # Post
+  # User Create and User Session Establishment
   post "/users" => "users#create"
+  post "/sessions" => "sessions#create"
 end
