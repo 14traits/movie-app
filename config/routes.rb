ae3bae517238e365782658e90dc587fa992a3569
John Rogers <john.rogers@mac.com>
2022-07-16 15:59:33 -0500

    auth part1

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,6 @@
 Rails.application.routes.draw do
   resources :actors, :movies, defaults: { format: :json }
 
-  # ## Actors
-  # # GET "/actors" - Gets all the actors we have
-  # get "/actors" => "actors#index"
-
-  # # POST "/actors" - Creates a new actor, add it to our database
-  # post "/actors" => "actors#create"
-
-  # # GET "/actors/3" - Just get one specific actor (that already exists)
-  # get "/actors/:id" => "actors#show"
-
-  # # PATCH "/actors/3" - Updates a specific actor
-  # patch "/actors/:id" => "actors#update"
-
-  # # DELETE "/actors/3" - Deletes a specific actor
-  # delete "/actors/:id" => "actors#destroy"
-
-  # ## Movies
-  # # GET "/movies" - Gets all the movies we have
-  # get "/movies" => "movies#index"
-
-  # # POST "/movies" - Creates a new movie, add it to our database
-  # post "/movies" => "movies#create"
-
-  # # GET "/movies/3" - Just get one specific movie (that already exists)
-  # get "/movies/:id" => "movies#show"
-
-  # # PATCH "/movies/3" - Updates a specific movie
-  # patch "/movies/:id" => "movies#update"
-
-  # # DELETE "/movies/3" - Deletes a specific movie
-  # delete "/movies/:id" => "movies#destroy"
+  # Post
+  post "/users" => "users#create"
 end
