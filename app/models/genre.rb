2de71662a4907e3ad9b79117f391de795891705e
John Rogers <john.rogers@mac.com>
2022-07-18 18:57:00 -0500

    ++ assocations for Movie, Genre, and Movie Genre models

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,4 @@
 class Genre < ApplicationRecord
+  has_many :movie_genre
+  has_many :moives, through: :movie_genre
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :movie_genre
+  has_many :genre, through: :movie_genre
   # validates :title, presence: true, length:
   # validates :director, presence: true, length: { minimum: 2 }
   # validates :plot, presence: true
