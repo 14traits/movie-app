c1be8b4e228605d0dffb3723a082635179e015f5
John Rogers <john.rogers@mac.com>
2022-07-18 19:16:34 -0500

    deleted bad views

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,4 +9,7 @@ class Movie < ApplicationRecord
   # # 1888. In Leeds, England Louis Le Prince films Roundhay Garden Scene, believed to be the first motion picture recorded.  https://en.wikipedia.org/wiki/List_of_cinematic_firsts
   # validates :year, presence: true, comparison: { greater_than_or_equal_to: 1888 }
 
+  def genre_names
+    return self.genres.map { |genre| genre.name }
+  end
 end
